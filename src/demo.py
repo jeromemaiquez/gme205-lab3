e08a447b925ad88f290065f96cd472b2ce722b3d
jeromemaiquez <maiquez.jerome.ml@gmail.com>
2026-02-14 23:41:10 +0800

    Implemented SpatialObject class and refactored Point class as child of SpatialObject + added test

--- src/demo.py
+++ src/demo.py
@@ -1,8 +1,10 @@
 from spatial import Point
 
 p = Point("A", 121.0, 14.6)
-print(p.id, p.geometry.x, p.geometry.y)
-print(p.to_tuple())
+print("BBox: ", p.bbox())
+print("Tuple: ", p.to_tuple())
+# print(p.id, p.geometry.x, p.geometry.y)
+# print(p.to_tuple())
 
-q = Point("B", 122.0, 15.6)
-print(p.distance_to(q))
+# q = Point("B", 122.0, 15.6)
+# print(p.distance_to(q))
